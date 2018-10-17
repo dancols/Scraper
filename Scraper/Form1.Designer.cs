0d29de8808fdc9e4c6306255d9e3a8f77e73789c
Dan Colosimo <dcolosi@mic-logistics.com>
2018-10-17 17:53:49 -0400

    Add project files.

---
+++ Scraper/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Scraper {
+    partial class frmScraper {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.lblLookupDate = new System.Windows.Forms.Label();
+            this.dteLookupDate = new System.Windows.Forms.DateTimePicker();
+            this.txtResults = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(164, 143);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(321, 14);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 1;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // lblLookupDate
+            // 
+            this.lblLookupDate.AutoSize = true;
+            this.lblLookupDate.Location = new System.Drawing.Point(15, 24);
+            this.lblLookupDate.Name = "lblLookupDate";
+            this.lblLookupDate.Size = new System.Drawing.Size(73, 13);
+            this.lblLookupDate.TabIndex = 2;
+            this.lblLookupDate.Text = "Look up date:";
+            // 
+            // dteLookupDate
+            // 
+            this.dteLookupDate.Location = new System.Drawing.Point(94, 17);
+            this.dteLookupDate.Name = "dteLookupDate";
+            this.dteLookupDate.Size = new System.Drawing.Size(212, 20);
+            this.dteLookupDate.TabIndex = 0;
+            // 
+            // txtResults
+            // 
+            this.txtResults.Location = new System.Drawing.Point(94, 59);
+            this.txtResults.Multiline = true;
+            this.txtResults.Name = "txtResults";
+            this.txtResults.Size = new System.Drawing.Size(212, 63);
+            this.txtResults.TabIndex = 2;
+            // 
+            // frmScraper
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(415, 176);
+            this.Controls.Add(this.txtResults);
+            this.Controls.Add(this.dteLookupDate);
+            this.Controls.Add(this.lblLookupDate);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.btnClose);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmScraper";
+            this.Text = "Get MXN Exchange Rate";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Label lblLookupDate;
+        private System.Windows.Forms.DateTimePicker dteLookupDate;
+        private System.Windows.Forms.TextBox txtResults;
+    }
+}
+
---
+++ Scraper/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+
+namespace Scraper {
+    public partial class frmScraper : Form {
+        private const string WebURL = "http://www.diariooficial.gob.mx/indicadores_detalle.php";
+        private const string MobileURL = "http://www.dof.gob.mx/DOFmobile/indicadores.php";
+                
+        public frmScraper() {
+            InitializeComponent();
+        }
+
+        private void btnGo_Click(object sender, EventArgs e) {
+            this.txtResults.Text = "Requesting...";
+            string exchangeRate = GetExchangeRate(this.dteLookupDate.Value);
+            this.txtResults.Text = exchangeRate;
+        }
+
+        private string GetExchangeRate(DateTime lookupDate) {
+            string url;
+            // url = GetWebURL(lookupDate.Day.ToString().PadLeft(2, '0'), lookupDate.Month.ToString().PadLeft(2, '0'), lookupDate.Year.ToString());
+            url = GetMobileURL(lookupDate.Day.ToString().PadLeft(2,'0'), lookupDate.Month.ToString().PadLeft(2, '0'), lookupDate.Year.ToString());
+            
+            WebRequest request = WebRequest.Create(url);
+            WebResponse response = request.GetResponse();
+
+            // string status = ((HttpWebResponse)response).StatusDescription;
+
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string content = reader.ReadToEnd();
+
+            reader.Close();
+            response.Close();
+
+            // Parse out the exchange rate (for MobileURL)
+            // snippet can be set by configuration (country code) in case the web page's markup is changed
+            string snippet = "<span class=\"unidades\"><b>DOLAR:</b> </span><span class=\"cantidad\">";
+
+            string exchangeRate;
+
+            if (content.Contains(snippet)) {
+                int start = content.IndexOf(snippet);
+                exchangeRate = content.Substring(start + snippet.Length, 9);
+            } else {
+                exchangeRate = "Exchange rate not found for " + lookupDate.ToShortDateString() + ".";
+            }
+
+            return exchangeRate;
+        }
+
+        private string GetWebURL(string day, string month, string year) {
+            // ?cod_tipo_indicador=158&dfecha=08%2F10%2F2018&hfecha=08%2F10%2F2018
+            string formattedDate = day + "%2F" + month + "%2F" + year;
+            string url = WebURL + "?cod_tipo_indicador=158&dfecha=" + formattedDate + "&hfecha=" + formattedDate;
+            return url;
+        }
+
+        private string GetMobileURL(string day, string month, string year) {
+            // ?cod_tipo_indicador=158&diaD=10&mesD=10&anioD=2018&diaH=10&mesH=10&anioH=2018
+            string formattedDate = "diaD=" + day + "&mesD=" + month + "&anioD=2018&";
+            formattedDate += "diaH=" + day + "&mesH=" + month + "&anioH=2018";
+            string url = MobileURL + "?cod_tipo_indicador=158&" + formattedDate;
+            return url;
+        }
+
+        private void btnClose_Click(object sender, EventArgs e) {
+            Application.Exit();
+        }
+
+    }
+}
